fb132ebae5e602d2bdbc1d1dbc75fd4523938391
Phillip Dykman <phil.d324@gmail.com>
2025-05-28 19:04:23 -0700

    Strip unnecessary content

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -2,35 +2,25 @@ import {themes as prismThemes} from 'prism-react-renderer';
 import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
-// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
-
 const config: Config = {
   title: 'Spaghetti Punch',
   tagline: 'Gamedev and Tech Blog',
   favicon: 'img/favicon.ico',
 
-  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
     v4: true, // Improve compatibility with the upcoming Docusaurus v4
   },
 
-  // Set the production url of your site here
   url: 'https://spaghettipunch.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
+  // GitHub pages deployment config
   organizationName: 'spaghettipunch', // Usually your GitHub org/user name.
   projectName: 'spaghettipunch.github.io', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
@@ -43,9 +33,7 @@ const config: Config = {
     [
       "@docusaurus/preset-classic",
       {
-        docs: {
-          sidebarPath: require.resolve("./sidebars.js"),
-        },
+        docs: false,
         blog: {
             showReadingTime: true,
             routeBasePath: '/blog',
@@ -78,7 +66,7 @@ const config: Config = {
       items: [
         {to: '/blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/electron/electron',
+          href: 'https://github.com/SpaghettiPunch',
           position: 'right',
           className: 'header-github-link',
           'aria-label': 'GitHub repository',
@@ -88,15 +76,6 @@ const config: Config = {
     footer: {
       style: 'dark',
       links: [
-        // {
-        //   title: 'Docs',
-        //   items: [
-        //     {
-        //       label: 'Tutorial',
-        //       to: '/docs/intro',
-        //     },
-        //   ],
-        // },
         // {
         //   title: 'Community',
         //   items: [
@@ -114,21 +93,8 @@ const config: Config = {
         //     },
         //   ],
         // },
-        // {
-        //   title: 'More',
-        //   items: [
-        //     {
-        //       label: 'Blog',
-        //       to: '/blog',
-        //     },
-        //     {
-        //       label: 'GitHub',
-        //       href: 'https://github.com/facebook/docusaurus',
-        //     },
-        //   ],
-        // },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} SpaghettiPunch`,
+      copyright: `Copyright © ${new Date().getFullYear()} Spaghetti Punch`,
     },
     prism: {
       theme: prismThemes.github,
